b4623829be8518a6914ac45e46745688c62a2693
liyu20 <liyu20@jd.com>
2018-07-11 17:02:01 +0800

    1、ImageEngine添加keep

--- mae-bundles-album/src/main/java/com/yu/bundles/album/image/ImageEngine.java
+++ mae-bundles-album/src/main/java/com/yu/bundles/album/image/ImageEngine.java
@@ -1,6 +1,7 @@
 package com.yu.bundles.album.image;
 
 import android.content.Context;
+import android.support.annotation.Keep;
 import android.widget.ImageView;
 
 import java.io.File;
@@ -9,6 +10,7 @@ import java.io.File;
  * Created by liyu on 2017/9/30.
  */
 
+@Keep
 public interface ImageEngine {
     File downloadFile(Context context, Object pathInfo);
     void loadImg(Context context, Object pathInfo, ImageView imageView, boolean isNeedPlaceHolder, AlbumEngineLoadListener... listeners);

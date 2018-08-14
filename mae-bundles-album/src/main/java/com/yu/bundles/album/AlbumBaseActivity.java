9279b43f9e71b87b53d98d70f95c200c348fc539
liyu20 <liyu20@jd.com>
2018-08-14 17:40:25 +0800

    更新状态栏显示

--- mae-bundles-album/src/main/java/com/yu/bundles/album/AlbumBaseActivity.java
+++ mae-bundles-album/src/main/java/com/yu/bundles/album/AlbumBaseActivity.java
@@ -1,14 +1,18 @@
 package com.yu.bundles.album;
 
-import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.yu.bundles.album.utils.MethodUtils;
 import com.yu.bundles.album.widget.MaeAlbumToolbar;
 
 public class AlbumBaseActivity extends AppCompatActivity{
@@ -16,20 +20,44 @@ public class AlbumBaseActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setTheme(ConfigBuilder.styleRes);
-        init(this);
+        init();
         super.onCreate(savedInstanceState);
     }
 
-    private void init(Activity activity){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            View decorView = activity.getWindow().getDecorView();
-            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-            decorView.setSystemUiVisibility(option);
+    private void init(){
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            getWindow().setStatusBarColor(Color.argb(55,0,0,0));
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            ViewGroup decorView = (ViewGroup) getWindow().getDecorView();
+            View statusBarView = new View(this);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(-1, getStatusBarHeight(this));
+            statusBarView.setLayoutParams(params);
+            statusBarView.setBackgroundColor(Color.argb(55,0,0,0));
+            decorView.addView(statusBarView);
+            ViewGroup parent = findViewById(android.R.id.content);
+            int i = 0;
+            for(int count = parent.getChildCount(); i < count; ++i) {
+                View childView = parent.getChildAt(i);
+                if (childView instanceof ViewGroup) {
+                    childView.setFitsSystemWindows(true);
+                    ((ViewGroup)childView).setClipToPadding(true);
+                }
+            }
         }
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
-            getWindow().setStatusBarColor(MethodUtils.getColorByAttrId(getApplicationContext(), R.attr.colorPrimaryDark));
-        }
+    }
+
+    private int getStatusBarHeight(Context context) {
+        if(context == null)
+            return 0;
+        // 获得状态栏高度
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        return context.getResources().getDimensionPixelSize(resourceId);
     }
 
     private void initToolbar(MaeAlbumToolbar toolbar){
